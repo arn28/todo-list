a1a60f56d2f53f5ef5792dda5c2b92690db80977
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-12-07 01:23:53 -0500

    feat: Added disabled prop to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,22 @@ import './Button.scss'
 
 interface IProps {
   children: React.ReactNode
+  disabled?: boolean
+  onCLick?: () => void
 }
-export const Button: React.FC<IProps> = ({ children }) => {
+export const Button: React.FC<IProps> = ({
+  children,
+  onCLick,
+  disabled = false,
+}) => {
   return (
-    <button type='submit' className='mainButton' data-form-btn>
+    <button
+      onClick={onCLick}
+      type='button'
+      className={`mainButton ${disabled ? 'disabled' : ''}`}
+      data-form-btn
+      disabled={disabled}
+    >
       {children}
     </button>
   )
