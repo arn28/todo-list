86a0e62d486096a6480691ab036024e20af312ac
arn28 <arnold.dev.me@gmail.com>
2024-03-06 15:08:06 -0500

    fix: Improved task container height and when to show scroll

--- src/features/Todo/TodoList/TodoList.tsx
+++ src/features/Todo/TodoList/TodoList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import { Task } from '../../../types/to-do'
 import { TodoItem } from '../TodoItem/TodoItem'
 import './TodoList.scss'
@@ -5,10 +6,19 @@ interface IProps {
   tasks: Task[]
 }
 export const TodoList: React.FC<IProps> = ({ tasks }) => {
+  const taskContainer = useRef<HTMLUListElement | null>(null)
+  const [showScroll, setShowScroll] = useState<boolean>(false)
+
+  useEffect(() => {
+    const taskContainerClientHeight = taskContainer.current?.clientHeight || 0
+    const taskContainerScrollHeight = taskContainer.current?.scrollHeight || 0
+    setShowScroll(taskContainerScrollHeight > taskContainerClientHeight)
+  }, [tasks])
+
   return (
     <ul
-      className={`cardsList ${tasks.length >= 9 ? 'extended' : ''}`}
-      data-list
+      ref={taskContainer}
+      className={`cardsList ${showScroll ? 'extended' : ''}`}
     >
       {tasks && tasks.map((task) => <TodoItem key={task.id} task={task} />)}
     </ul>
